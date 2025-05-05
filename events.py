d5c94a9e057c111bdea0f630d6ac06cd8b3bf4d1
unknown <redrosovich@gmail.com>
2025-05-05 19:12:02 +0300

    added events

---
+++ events.py
@@ -0,0 +1,21 @@
+# events.py
+import discord
+from database import BotDatabase
+from utils import DAILY_COOLDOWN
+
+def setup(bot):
+    @bot.event
+    async def on_ready():
+        print(f"Logged in as {bot.user}")
+    
+    @bot.event
+    async def on_message(message):
+        if not message.author.bot:
+            db.update_message_count(str(message.author.id))
+            db.update_experience(str(message.author.id), 5)
+            await bot.process_commands(message)
+    
+    @bot.event
+    async def on_reaction_add(reaction, user):
+        if not user.bot:
+            db.update_emoji_usage(str(user.id), str(reaction.emoji))
